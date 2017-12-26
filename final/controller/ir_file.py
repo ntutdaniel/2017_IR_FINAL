5ba07c714fe4a0e1b35140169007c9d87897d06d
elena40628 <34837259+elena40628@users.noreply.github.com>
2017-12-26 16:38:28 +0800

    ir_file_change_tw
    tw-idf_paper_change

--- final/controller/ir_file.py
+++ final/controller/ir_file.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 import os
-
-
+import time
 # calculate the word count in document
 def ReadFile(path, start_index):
     word_counter = {}
+    word_tw_grah = {}       #建圖 [word3]={word1,word2}
+    word_tw = {}            #從上面的圖取得in_degree數量
+    TW_WINDOW_SIZE = 2      #不包含自己 看前幾個
     document_info = []
     document_word_count = 0
 
@@ -14,14 +16,31 @@ def ReadFile(path, start_index):
         if (i < start_index):
             document_info.append(word_list)
         else:
-            for word in word_list.split():
+            #for word in word_list.split():
+            for index,word in enumerate(word_list.split()):
+                #print(word_list.split())
                 temp = str(word)
                 document_word_count += 1
                 if temp not in word_counter:
                     word_counter[temp] = 1
                 else:
                     word_counter[temp] += 1
-    return document_info, word_counter, document_word_count
+                    
+                    #--- add_for_tw
+                if(index>=TW_WINDOW_SIZE):
+                    for k in range(index-TW_WINDOW_SIZE,index):
+                        #print(index,word,word_list.split()[k])                        
+                        temp_addtw=word_list.split()[k]
+                        if temp in word_tw_grah :                            
+                            word_tw_grah[temp].add(temp_addtw)
+                        else:
+                            word_tw_grah[temp]={temp_addtw}
+                            
+                    word_tw[temp]=len(word_tw_grah[temp])
+                
+                
+                    
+    return document_info, word_counter, document_word_count, word_tw
 
 
 # debug
@@ -37,6 +56,7 @@ def ReadFileDebug(document_info, word_counter):
 
 def ReadFolder(p, start_index):
     folder = {}
+    folder_tw={}
     files = os.listdir(p)
     files_name = []
     folder_word_counter = {}
@@ -51,9 +71,10 @@ def ReadFolder(p, start_index):
     for fn in files_name:
         fullpath = os.path.join(p, fn)
         if (fn == '.DS_Store'): continue
-        document_info, word_counter, document_word_count = ReadFile(fullpath, start_index)
+        document_info, word_counter, document_word_count, word_tw = ReadFile(fullpath, start_index)
 
         folder[fn] = word_counter
+        folder_tw[fn] = word_tw
         # debug
         # if debug: ReadFileDebug(document_info, word_counter)
 
@@ -70,7 +91,7 @@ def ReadFolder(p, start_index):
                 folder_word_counter_distince[word] += 1
 
     # doc word count/ folder world count/ folder world distinct count
-    return folder, folder_word_counter, folder_word_counter_distince
+    return folder, folder_word_counter, folder_word_counter_distince,folder_tw
 
 
 def ReadFolderDebug(po, doc_word_count, folder_word_count, folder_word_count_distinct):
@@ -127,23 +148,24 @@ if __name__ == '__main__':
     debug3 = False
 
     p = '../dataset/Query/20001.query'
-    pd = '../dataset/Document'
+    pd = '../dataset/Document_final'
     po = '../dataset/Output'
-    pq = '../dataset/Query'
+    pq = '../dataset/TestQuery_final'
+    
     start_index = 3
 
     # file
-    document_info, word_counter, document_word_count = ReadFile(p, start_index)
+    #document_info, word_counter, document_word_count = ReadFile(p, start_index)
     # debug function
     if debug1: ReadFileDebug(document_info, word_counter)
 
     # documents
-    doc_word_count, folder_word_count, folder_word_count_distinct = ReadFolder(pd, start_index)
+    doc_word_count, folder_word_count, folder_word_count_distinct,doc_folder_tw = ReadFolder(pd, start_index)
     # debug function
-    if debug2: ReadFolderDebug(po, doc_word_count, folder_word_count, folder_word_count_distinct)
+    if debug2: ReadFolderDebug(po, doc_word_count, folder_word_count, folder_word_count_distinct,doc_folder_tw)
 
     # query
     start_index = 0
-    doc_word_count, folder_word_count, folder_word_count_distinct = ReadFolder(pq, start_index)
+    doc_word_count, folder_word_count, folder_word_count_distinct,doc_folder_tw = ReadFolder(pq, start_index)
     # debug function
-    if debug3: ReadFolderDebug(po, doc_word_count, folder_word_count, folder_word_count_distinct)
+    if debug3: ReadFolderDebug(po, doc_word_count, folder_word_count, folder_word_count_distinct,doc_folder_tw)
